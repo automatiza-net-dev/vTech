a7f5d02bdf237985f8052d0ad223ebd76944d55b
automatiza-net-dev <automatiza.net.dev@gmail.com>
2025-03-19 17:54:37 -0300

    v

--- src/presentation/pages/dashboard/scheduling/context/components/modal-create-scheduling/form-create-scheduling/index.tsx
+++ src/presentation/pages/dashboard/scheduling/context/components/modal-create-scheduling/form-create-scheduling/index.tsx
@@ -5,6 +5,7 @@ import {
   Textarea,
   FormHandler,
   useAuthAdmin,
+  InputDatePicker,
 } from "infinity-forge";
 
 import { useScheduling } from "@/presentation";
@@ -92,8 +93,6 @@ export function FormCreateScheduling() {
     value: user.id,
   }));
 
-  console.log(moment(createSchedulingArgs?.date).format("YYYY-MM-DD"))
-
   return (
     <S.FormCreateScheduling>
       <FormHandler
@@ -158,7 +157,7 @@ export function FormCreateScheduling() {
         <h3>Data e horário</h3>
 
         <div className="row">
-          <Input
+          <InputDatePicker
             type="date"
             name="date"
             readOnly={createSchedulingArgs?.type === "edit"}
