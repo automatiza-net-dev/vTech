4d6f5874ac1fda5396d249202f101927ea711f77
codie.technology <codie.technology@outlook.com>
2025-05-12 23:26:45 -0300

    v

--- src/pages/dashboard/tipo-venda-relacionada/index.tsx
+++ src/pages/dashboard/tipo-venda-relacionada/index.tsx
@@ -1,9 +1,170 @@
-import { LayoutDashboard } from "@/presentation";
+import {
+  api,
+  useQuery,
+  useTable,
+  useToast,
+  BadRequestError,
+} from "infinity-forge";
+
+import {
+  useSystem,
+  AccessDenied,
+  usePermission,
+  PermissionItem,
+  LayoutDashboard,
+} from "@/presentation";
+
+import * as yup from "yup";
 
 export default function TipoVendaRelacionada() {
-
-
-    return <LayoutDashboard>
-        <>a</>
+  return (
+    <LayoutDashboard>
+      <Page />
     </LayoutDashboard>
+  );
+}
+
+function Page() {
+  const deletePermission = usePermission("VRL03");
+  const createPermission = usePermission("VRL01");
+  const updatePermission = usePermission("VRL02");
+
+  const { createToast } = useToast();
+  const { unit } = useSystem();
+
+  const economicGroupId = unit?.economicGroup?.id;
+
+  const { data, isFetching, mutate } = useQuery({
+    queryKey: ["bill-related-types"],
+    queryFn: async () => {
+      const response = await api({ url: "bill-related-types", method: "get" });
+
+      return response as { id: string; description: string; active: boolean }[];
+    },
+  });
+
+  const { Table } = useTable({
+    columnsConfiguration: {
+      columns: [
+        { id: "description", label: "Descrição" },
+        {
+          id: "active",
+          label: "ATIVO",
+          Component: { Element: (props) => (props.active ? "Sim" : "Não") },
+        },
+      ],
+      actions: {
+        create: createPermission
+          ? {
+              text: "Criar novo",
+              disableEnterKeySubmitForm: true,
+              isStickyButtons: true,
+              button: { text: "Salvar" },
+              schema: { description: yup.string().required("Campo requerido") },
+              onSucess: async (formData) => {
+                const response = await api({
+                  url: "bill-related-types/store",
+                  method: "post",
+                  body: { economicGroupId, description: formData?.description },
+                });
+
+                await mutate();
+
+                createToast({
+                  status: "success",
+                  message: "Registro criado com sucesso",
+                });
+              },
+              inputs: [
+                [
+                  {
+                    label: "Descrição",
+                    InputComponent: "Input",
+                    name: "description",
+                  },
+                ],
+              ],
+            }
+          : undefined,
+        edit: updatePermission
+          ? {
+              disableEnterKeySubmitForm: true,
+              isStickyButtons: true,
+              button: { text: "Salvar" },
+              initialDataIsTableItem: true,
+              schema: { description: yup.string().required("Campo requerido") },
+              onSucess: async (formData) => {
+                const response = await api({
+                  url: "bill-related-types/update",
+                  method: "put",
+                  body: {
+                    id: formData?.id,
+                    active: formData?.active,
+                    economicGroupId,
+                    description: formData?.description,
+                  },
+                });
+
+                await mutate();
+
+                createToast({
+                  status: "success",
+                  message: "Registro criado com sucesso",
+                });
+              },
+              inputs: [
+                [
+                  {
+                    label: "Ativo",
+                    InputComponent: "InputSwitch",
+                    name: "active",
+                  },
+                ],
+                [
+                  {
+                    label: "Descrição",
+                    InputComponent: "Input",
+                    name: "description",
+                  },
+                ],
+              ],
+            }
+          : undefined,
+        delete: deletePermission
+          ? {
+              confirmDelete: true,
+              onDelete: async (props) => {
+                try {
+                  await api({
+                    url: `bill-related-types/delete/${props.id}`,
+                    method: "delete",
+                  });
+
+                  await mutate();
+                } catch (err) {
+                  if (err instanceof BadRequestError) {
+                    createToast({
+                      status: "warning",
+                      message: err.error.message,
+                      duration: 10000,
+                    });
+                  }
+                }
+              },
+            }
+          : undefined,
+      },
+    },
+    configs: {
+      tableData: data || [],
+      errorMessage: "Não há tipo de venda relacionada cadastrada",
+      isLoading: isFetching,
+    },
+  });
+
+  return (
+    <PermissionItem hash="VRL00" DaniedComponent={AccessDenied}>
+      {Table}
+    </PermissionItem>
+  );
 }
