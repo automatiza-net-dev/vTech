201fbd5b7b34953ee16762d4fed4735497b20124
codie.technology <codie.technology@outlook.com>
2025-05-29 14:50:32 -0300

    v

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,11 +10,12 @@ import {
   BadRequestError,
   InfinityForgeProviders,
   useAuthAdmin,
+
 } from "infinity-forge";
 
-import { useQuery } from "@/presentation/use-query";
+import {useQuery} from "@/presentation/use-query"
 
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient } from '@tanstack/react-query';
 
 import { ConfigProvider } from "antd";
 import ptBR from "antd/lib/locale/pt_BR";
@@ -46,9 +47,10 @@ import "infinity-forge/dist/infinity-forge.css";
 import Link from "next/link";
 import { PermissionsProvider } from "@/presentation/context/permissions";
 
-import { QueryClient as QueryClientInfinity } from "infinity-query";
+import { QueryClient as QueryClientInfinity } from "infinity-query"
+import { QueryClientContextProvider } from "@/presentation/use-query/use-query/context";
 
-const queryClientInfinity = new QueryClientInfinity();
+  const queryClientInfinity = new QueryClientInfinity()
 
 export default function App({ Component, pageProps }) {
   const [menus, setMenus] = useState<any>(null);
@@ -62,165 +64,167 @@ export default function App({ Component, pageProps }) {
   }
 
   return (
-    <ConfigurationsSystemProvider configurations={configurations}>
-      <InfinityForgeProviders
-        queryClient={queryClientInfinity}
-        atena={{ disableAuth: true, roles: ["aa"] } as any}
-        i18n={{ roleToEditLanguage: ["aa"], disableEditMode: true } as any}
-        auth={{
-          ForbiddenCompoent: Forbidden,
-          roles: {
-            user: {
-              signInConfig: { Component: SignIn },
-              onSignOut: (user: any) => {
-                router.push("/");
+    <QueryClientContextProvider>
+      <ConfigurationsSystemProvider configurations={configurations}>
+        <InfinityForgeProviders
+          queryClient={queryClientInfinity}
+          atena={{ disableAuth: true, roles: ["aa"] } as any}
+          i18n={{ roleToEditLanguage: ["aa"], disableEditMode: true } as any}
+          auth={{
+            ForbiddenCompoent: Forbidden,
+            roles: {
+              user: {
+                signInConfig: { Component: SignIn },
+                onSignOut: (user: any) => {
+                  router.push("/");
 
-                if (user?.isThirdParty) {
-                  window.location.href =
-                    "https://portal.liftonefranquias.com.br/";
+                  if (user?.isThirdParty) {
+                    window.location.href =
+                      "https://portal.liftonefranquias.com.br/";
+                  }
+                },
+              },
+              controller: {
+                signInConfig: {
+                  Component: SignInAdmin,
+                },
+                onSignOut: () => {
+                  router.push("/");
+                },
+              },
+            },
+          }}
+          loaderOnRouteChange={{ Component: LoaderOnRouteChange } as any}
+          InjectedRemotes={{
+            menu: {
+              menu: menus || ({ items: [] } as any),
+            } as any,
+            user: {
+              getRole: async () => {
+                try {
+                  const user = await container
+                    .get<RemoteLoadUserDashboard>(
+                      TypesAutomatiza.RemoteLoadUserDashboard
+                    )
+                    .load({});
+
+                  if (!user?.user) {
+                    throw new BadRequestError({
+                      message: "Usuário com erro",
+                      code: "400",
+                    });
+                  }
+
+                  const initialUserData = {
+                    ...user,
+                    avatar: user.user?.profile_picture || "",
+                    emailAddress: user?.user?.email || "",
+                    firstName: user?.user?.name || "",
+                    id: (user as any)?.user?.id || "",
+                    imagem: user.user?.profile_picture,
+                    isExternal: false,
+                    lastName: "",
+                  };
+
+                  return {
+                    role: initialUserData?.user?.type,
+                    user: initialUserData,
+                  };
+                } catch (err) {
+                  return { role: "", user: null };
                 }
               },
             },
             controller: {
-              signInConfig: {
-                Component: SignInAdmin,
-              },
-              onSignOut: () => {
-                router.push("/");
-              },
-            },
-          },
-        }}
-        loaderOnRouteChange={{ Component: LoaderOnRouteChange } as any}
-        InjectedRemotes={{
-          menu: {
-            menu: menus || ({ items: [] } as any),
-          } as any,
-          user: {
-            getRole: async () => {
-              try {
-                const user = await container
-                  .get<RemoteLoadUserDashboard>(
-                    TypesAutomatiza.RemoteLoadUserDashboard
-                  )
-                  .load({});
+              getRole: async () => {
+                try {
+                  const user = await container
+                    .get<RemoteLoadUserDashboard>(
+                      TypesAutomatiza.RemoteLoadUserDashboard
+                    )
+                    .load({});
 
-                if (!user?.user) {
-                  throw new BadRequestError({
-                    message: "Usuário com erro",
-                    code: "400",
-                  });
+                  if (!user?.user) {
+                    throw new BadRequestError({
+                      message: "Usuário com erro",
+                      code: "400",
+                    });
+                  }
+
+                  const initialUserData = {
+                    ...user,
+                    avatar: user.user?.profile_picture || "",
+                    emailAddress: user?.user?.email || "",
+                    firstName: user?.user?.name || "",
+                    id: (user as any)?.user?.id || "",
+                    imagem: user.user?.profile_picture,
+                    isExternal: false,
+                    lastName: "",
+                  };
+
+                  return {
+                    role: initialUserData?.user?.type,
+                    user: initialUserData,
+                  };
+                } catch (err) {
+                  return { role: "", user: null };
                 }
-
-                const initialUserData = {
-                  ...user,
-                  avatar: user.user?.profile_picture || "",
-                  emailAddress: user?.user?.email || "",
-                  firstName: user?.user?.name || "",
-                  id: (user as any)?.user?.id || "",
-                  imagem: user.user?.profile_picture,
-                  isExternal: false,
-                  lastName: "",
-                };
-
-                return {
-                  role: initialUserData?.user?.type,
-                  user: initialUserData,
-                };
-              } catch (err) {
-                return { role: "", user: null };
-              }
+              },
             },
-          },
-          controller: {
-            getRole: async () => {
-              try {
-                const user = await container
-                  .get<RemoteLoadUserDashboard>(
-                    TypesAutomatiza.RemoteLoadUserDashboard
-                  )
-                  .load({});
-
-                if (!user?.user) {
-                  throw new BadRequestError({
-                    message: "Usuário com erro",
-                    code: "400",
-                  });
-                }
-
-                const initialUserData = {
-                  ...user,
-                  avatar: user.user?.profile_picture || "",
-                  emailAddress: user?.user?.email || "",
-                  firstName: user?.user?.name || "",
-                  id: (user as any)?.user?.id || "",
-                  imagem: user.user?.profile_picture,
-                  isExternal: false,
-                  lastName: "",
-                };
-
-                return {
-                  role: initialUserData?.user?.type,
-                  user: initialUserData,
-                };
-              } catch (err) {
-                return { role: "", user: null };
-              }
+          }}
+          Configurations={{
+            chat: false,
+            menu: {
+              mode: "CollapsedMenu",
             },
-          },
-        }}
-        Configurations={{
-          chat: false,
-          menu: {
-            mode: "CollapsedMenu",
-          },
-          styles: { Button: ButtonInfinityForge },
-          notification: {
-            enable: true,
-            CustomComponent: (props: any) => (
-              <Link href={props?.link}>
-                <div className="top">
-                  <h3>{props?.title}</h3> <span>{props?.createdAtText}</span>
-                  <span>{props?.message}</span>
-                </div>
-              </Link>
-            ),
-          },
-        }}
-        theme={{
-          black: "#000",
-          red: "#ef1717",
-          green: "#39b15d",
-          orange: "#f18805",
-          yellow: "#e1b400",
-          secondaryColor: "red",
-          darkColor: "#2B2B2B",
-          primaryColor: configurations?.primary_color || "#000",
-        }}
-      >
-        <GlobalStyles host={configurations.name} />
+            styles: { Button: ButtonInfinityForge },
+            notification: {
+              enable: true,
+              CustomComponent: (props: any) => (
+                <Link href={props?.link}>
+                  <div className="top">
+                    <h3>{props?.title}</h3> <span>{props?.createdAtText}</span>
+                    <span>{props?.message}</span>
+                  </div>
+                </Link>
+              ),
+            },
+          }}
+          theme={{
+            black: "#000",
+            red: "#ef1717",
+            green: "#39b15d",
+            orange: "#f18805",
+            yellow: "#e1b400",
+            secondaryColor: "red",
+            darkColor: "#2B2B2B",
+            primaryColor: configurations?.primary_color || "#000",
+          }}
+        >
+          <GlobalStyles host={configurations.name} />
 
-        <SchedulingContextProvider>
-          <LocalizationProvider dateAdapter={AdapterMoment}>
-            <ConfigProvider locale={ptBR}>
-              <AppProvider>
-                <PermissionsProvider>
-                  <Head>
-                    <title>{configurations.name}</title>
-                  </Head>
+          <SchedulingContextProvider>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+              <ConfigProvider locale={ptBR}>
+                <AppProvider>
+                  <PermissionsProvider>
+                    <Head>
+                      <title>{configurations.name}</title>
+                    </Head>
 
-                  <NotificationsModal />
+                    <NotificationsModal />
 
-                  <GambiarraTemporaria setMenus={setMenus} />
+                    <GambiarraTemporaria setMenus={setMenus} />
 
-                  <Component {...pageProps} />
-                </PermissionsProvider>
-              </AppProvider>
-            </ConfigProvider>
-          </LocalizationProvider>
-        </SchedulingContextProvider>
-      </InfinityForgeProviders>
-    </ConfigurationsSystemProvider>
+                    <Component {...pageProps} />
+                  </PermissionsProvider>
+                </AppProvider>
+              </ConfigProvider>
+            </LocalizationProvider>
+          </SchedulingContextProvider>
+        </InfinityForgeProviders>
+      </ConfigurationsSystemProvider>
+    </QueryClientContextProvider>
   );
 }
 
