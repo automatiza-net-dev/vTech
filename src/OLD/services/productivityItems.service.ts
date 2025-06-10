cfb50a69049efeadb3d6688fafea9ec28643f824
STNeto1 <gfreitasneto18@gmail.com>
2025-06-10 17:26:59 -0300

    feat: stuff

--- src/OLD/services/productivityItems.service.ts
+++ src/OLD/services/productivityItems.service.ts
@@ -1,6 +1,51 @@
 import api from "@/OLD/services";
 
-const getProductivityItems = async (params) =>
-  api.get("/productivity-items/products", { params });
+const getProductivityProducts = async (params) =>
+	api.get("/productivity-items/products", { params });
 
-export const productivityItemsService = { getProductivityItems };
+const getProductivityItems = async (params: Record<string, string>) =>
+	api.get<
+		{
+			id: number;
+			description: string;
+			active: boolean;
+			created_at: string;
+			updated_at: string;
+			reserved_minutes: number;
+			type_qty: number | null;
+			order: number;
+		}[]
+	>("/productivity-items/items", { params });
+
+const createProductivityItem = async (data: {
+	description: string;
+	reservedMinutes: number;
+	origin: string;
+	order: number;
+}) => api.post("/productivity-items/create-item", data);
+
+const updateProductivityItem = async (data: {
+	id: number;
+	description: string;
+	reservedMinutes: number;
+	origin: string;
+	order: number;
+	active: boolean;
+}) => api.post("/productivity-items/update-item", data);
+
+const createProductivityProduct = async (data: {
+	items: {
+		productivityItemId: string;
+		productId: string;
+		quantity: number;
+		order: number;
+	}[];
+}) => api.post("/productivity-items/create-item-product", data);
+
+export const productivityItemsService = {
+	getProductivityProducts,
+	getProductivityItems,
+	createProductivityItem,
+	updateProductivityItem,
+	createProductivityProduct,
+};
