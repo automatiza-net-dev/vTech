522d20f4d082b755387c49c54d8899ffd385a9bc
STNeto1 <gfreitasneto18@gmail.com>
2025-06-11 17:06:44 -0300

    fix: bad code

--- src/presentation/pages/dashboard/paciente/detail/components/actions/actions/video-photo/index.tsx
+++ src/presentation/pages/dashboard/paciente/detail/components/actions/actions/video-photo/index.tsx
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 
 import { useRouter } from "next/router";
 import {
-  Error,
-  FormHandler,
-  Input,
-  useToast,
-  Textarea,
-  InputFile,
-  useAuthAdmin,
-  api,
-  FileSystemType,
+	Error,
+	FormHandler,
+	Input,
+	useToast,
+	Textarea,
+	InputFile,
+	useAuthAdmin,
+	api,
+	FileSystemType,
 } from "infinity-forge";
 
 import { DropdownComponentProps } from "../dropdown-item";
@@ -19,96 +19,126 @@ import * as S from "./styles";
 import { useQueryClient } from "infinity-forge";
 
 export function VideoPhoto({ setModal, ...rest }: DropdownComponentProps) {
-  const [loading, setLoading] = useState(false);
-  const [photos, setPhotos] = useState<FileSystemType[] | null>(null);
+	console.log({ rest });
+	const [loading, setLoading] = useState(false);
+	const [photos, setPhotos] = useState<FileSystemType[] | null>(null);
 
-  const { createToast } = useToast();
+	const { createToast } = useToast();
 
-  const router = useRouter();
-  const userId = useAuthAdmin().user.id;
+	const router = useRouter();
+	const userId = useAuthAdmin().user.id;
 
-  const patientId = router.query.id as string;
+	const patientId = router.query.id as string;
 
-  const {refetch} = useQueryClient();
+	const { refetch } = useQueryClient();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        if(!rest.timeline_info) {
-          return;
-        }
+	useEffect(() => {
+		(async () => {
+			try {
+				if (!rest.timeline_info) {
+					return;
+				}
 
-        setLoading(true);
+				setLoading(true);
 
-        let photos = rest.timeline_info?.photos || [];
-        let newPhotos: any[] = [];
+				let photos = rest.timeline_info?.photos || [];
+				let newPhotos: any[] = [];
 
-        for (const photo of photos) {
-          const response = await api({
-            url: "s3/generate-link",
-            method: "post",
-            body: { key: photo.url },
-          });
+				for (const photo of photos) {
+					const response = await api({
+						url: "s3/generate-link",
+						method: "post",
+						body: { key: photo.url },
+					});
 
-          newPhotos.push({ ...photo, url: response.view });
-        }
+					newPhotos.push({ ...photo, url: response.view });
+				}
 
-        setPhotos(newPhotos);
-      } catch {
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, []);
+				setPhotos(newPhotos);
+			} catch {
+			} finally {
+				setLoading(false);
+			}
+		})();
+	}, []);
 
-  return (
-    <Error name="VideoPhoto">
-      <S.VideoPhoto>
-        {!loading && (
-          <FormHandler
-            button={{ text: "Salvar" }}
-            initialData={{ ...(rest?.timeline_info || {}), photos }}
-            onSucess={async (data) => {
-              const payload = {
-                title: data.title,
-                observation: data.observation,
-                photos: (data?.photos as FileSystemType[])?.map(
-                  (item) => item.file
-                ),
-                tag: patientId,
-                technicianId: userId,
-              };
+	return (
+		<Error name="VideoPhoto">
+			<S.VideoPhoto>
+				{!loading && (
+					<FormHandler
+						button={{ text: "Salvar" }}
+						initialData={{ ...(rest?.timeline_info || {}), photos }}
+						onSucess={async (data) => {
+							const payload = {
+								title: data.title,
+								observation: data.observation,
+								photos: (data?.photos as FileSystemType[])
+									?.map((item) => item.file)
+									.filter(Boolean),
+								tag: patientId,
+								technicianId: userId,
+							};
 
-              await api({
-                url: "n-timeline/photos",
-                method: "post",
-                body: payload,
-                headers: {
-                  "Content-Type": "multipart/form-data; boundary=something",
-                },
-              });
+							if (rest._id) {
+								// needs to update
+								await api({
+									url: `n-timeline/photos/${rest._id}`,
+									method: "put",
+									body: payload,
+									headers: {
+										"Content-Type": "multipart/form-data; boundary=something",
+									},
+								});
 
-              await refetch(["LastUpdates"], { mode: "include" });
+								if (payload.photos?.length > 0) {
+									console.log(payload.photos);
+									await api({
+										url: `n-timeline/photos/attachments/${rest._id}`,
+										method: "post",
+										body: { files: payload.photos },
+										headers: {
+											"Content-Type": "multipart/form-data; boundary=something",
+										},
+									});
+								}
 
-              createToast({
-                message: "Video/Foto criada com sucesso!",
-                status: "success",
-              });
+								createToast({
+									message: "Video/Foto atualizado com sucesso!",
+									status: "success",
+								});
+							} else {
+								await api({
+									url: "n-timeline/photos",
+									method: "post",
+									body: payload,
+									headers: {
+										"Content-Type": "multipart/form-data; boundary=something",
+									},
+								});
 
-              setModal && setModal(false);
-            }}
-            cleanFieldsOnSubmit={false}
-            isStickyButtons
-            disableEnterKeySubmitForm
-          >
-            <Input name="title" label="Titulo" />
+								createToast({
+									message: "Video/Foto criada com sucesso!",
+									status: "success",
+								});
+							}
 
-            <Textarea name="observation" label="Observações" />
+							await refetch(["LastUpdates"], { mode: "include" });
 
-            <InputFile name="photos" isAccumalativeFile multiple isLocalFile />
-          </FormHandler>
-        )}
-      </S.VideoPhoto>
-    </Error>
-  );
+							setModal && setModal(false);
+						}}
+						cleanFieldsOnSubmit={false}
+						isStickyButtons
+						disableEnterKeySubmitForm
+					>
+						<Input name="title" label="Titulo" />
+
+						<Textarea name="observation" label="Observações" />
+
+						<InputFile name="photos" isAccumalativeFile multiple isLocalFile />
+					</FormHandler>
+				)}
+			</S.VideoPhoto>
+		</Error>
+	);
 }
