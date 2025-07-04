92408488d0e59fe51e289cc27b3ee4c110031b85
STNeto1 <gfreitasneto18@gmail.com>
2025-07-04 12:51:34 -0300

    feat: table data

--- src/OLD/components/Banking/Columns/index.tsx
+++ src/OLD/components/Banking/Columns/index.tsx
@@ -1,37 +1,53 @@
 export const Columns = [
-  {
-    title: "Documento",
-    key: "document",
-    dataIndex: "document"
-  },
-  {
-    title: "Tipo",
-    key: "type",
-    dataIndex: "type"
-  },
-  {
-    title: "Conta",
-    key: "account",
-    dataIndex: "account"
-  },
-  {
-    title: "Histórico",
-    key: "historic",
-    dataIndex: "historic"
-  },
-  {
-    title: "Data",
-    key: "date",
-    dataIndex: "date"
-  },
-  {
-    title: "Valor",
-    key: "value",
-    dataIndex: "value"
-  },
-  {
-    title: "Ações",
-    key: "actions",
-    dataIndex: "actions"
-  }
+	{
+		title: "Documento",
+		key: "document",
+		dataIndex: "document",
+	},
+	{
+		title: "Tipo",
+		key: "type",
+		dataIndex: "type",
+	},
+	{
+		title: "Forma Pagamento",
+		key: "payment",
+		dataIndex: "payment",
+	},
+	{
+		title: "Bandeira Tef",
+		key: "bandeira",
+		dataIndex: "flag",
+	},
+	{
+		title: "Adquirente Tef",
+		key: "adquirente",
+		dataIndex: "acquirer",
+	},
+
+	{
+		title: "Conta",
+		key: "account",
+		dataIndex: "account",
+	},
+	{
+		title: "Histórico",
+		key: "historic",
+		dataIndex: "historic",
+	},
+	{
+		title: "Data",
+		key: "date",
+		dataIndex: "date",
+	},
+	{
+		title: "Valor",
+		key: "value",
+		dataIndex: "value",
+	},
+	{
+		title: "Ações",
+		key: "actions",
+		dataIndex: "actions",
+	},
 ];
