5910a7894d2f30829432d6b8e8d838cbb0a6caeb
STNeto1 <gfreitasneto18@gmail.com>
2025-07-10 10:08:30 -0300

    fix: type

--- src/OLD/components/Attendance/Forms-old/AddExam/FormChild.tsx
+++ src/OLD/components/Attendance/Forms-old/AddExam/FormChild.tsx
@@ -72,7 +72,7 @@ export default function FormChild({
 
   return (
     <Container>
-      <FormHandler isStickyButtons>
+      <FormHandler isStickyButtons disableEnterKeySubmitForm>
         <div>
           {modal ? (
             <>
