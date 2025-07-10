b2522e7c6193b2348dcaaa4922a8672eabfaaea7
STNeto1 <gfreitasneto18@gmail.com>
2025-07-10 10:08:30 -0300

    fix: type

--- src/OLD/hooks/useProductivityItems.ts
+++ src/OLD/hooks/useProductivityItems.ts
@@ -2,30 +2,40 @@ import { useEffect, useState } from "react";
 import { productivityItemsService } from "@/OLD/services/productivityItems.service";
 
 export const useProductivityItems = (productID: string) => {
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(false);
+	const [items, setItems] = useState<
+		{
+			id: number;
+			p_id: number;
+			description: string;
+			quantity: number;
+			reserved_minutes: number;
+			order: number;
+			active: boolean;
+		}[]
+	>([]);
+	const [loading, setLoading] = useState(false);
 
-  const fetchData = () => {
-    if (!productID) {
-      return;
-    }
+	const fetchData = () => {
+		if (!productID) {
+			return;
+		}
 
-    setLoading(true);
+		setLoading(true);
 
-    productivityItemsService
-      .getProductivityProducts({ product: productID })
-      .then((res) => setItems(res.data))
-      .catch(() => setLoading(false))
-      .finally(() => setLoading(false));
-  };
+		productivityItemsService
+			.getProductivityProducts({ product: productID })
+			.then((res) => setItems(res.data))
+			.catch(() => setLoading(false))
+			.finally(() => setLoading(false));
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, [productID]);
+	useEffect(() => {
+		fetchData();
+	}, [productID]);
 
-  return {
-    items,
-    loadingItems: loading,
-    fetchItems: fetchData,
-  };
+	return {
+		items,
+		loadingItems: loading,
+		fetchItems: fetchData,
+	};
 };
