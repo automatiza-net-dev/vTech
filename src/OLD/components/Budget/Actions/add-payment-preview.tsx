56bc4e4b6687fa57eea08fdfb403ae2afe0d373a
STNeto1 <gfreitasneto18@gmail.com>
2025-08-14 15:38:24 -0300

    fix: button style

--- src/OLD/components/Budget/Actions/add-payment-preview.tsx
+++ src/OLD/components/Budget/Actions/add-payment-preview.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Modal, Tooltip } from "infinity-forge";
+import { Modal, Tooltip, Button } from "infinity-forge";
 
 import { MdMonetizationOn } from "react-icons/md";
 import Negotiation from "@/OLD/components/Budget/Negotiation";
@@ -7,46 +7,35 @@ import Negotiation from "@/OLD/components/Budget/Negotiation";
 import { useDictionary, usePermission } from "@/presentation";
 
 export default function AddPaymentPreview({
-  budgetId,
-  budgetTag,
-  onUpdatePayment
+	budgetId,
+	budgetTag,
+	onUpdatePayment,
 }: {
-  onUpdatePayment?: () => void;
-  budgetId: string;
-  budgetTag: string;
+	onUpdatePayment?: () => void;
+	budgetId: string;
+	budgetTag: string;
 }) {
-  const hasPermission = usePermission("ORC02");
-  const [visible, setVisible] = useState(false);
+	const hasPermission = usePermission("ORC02");
+	const [visible, setVisible] = useState(false);
 
-  const { getWord } = useDictionary();
+	const { getWord } = useDictionary();
 
-  if (!budgetTag || !budgetId || !hasPermission) {
-    return <></>;
-  }
+	if (!budgetTag || !budgetId || !hasPermission) {
+		return <></>;
+	}
 
-  return (
-    <>
-      <Tooltip
-        idTooltip="add-payment-prev"
-        content="Lançar Pagamentos"
-        enableHover
-        trigger={
-          <MdMonetizationOn
-            onClick={() => setVisible(true)}
-            size={20}
-            className="icon"
-          />
-        }
-      />
+	return (
+		<>
+			<Button text="Pagamentos" onClick={() => setVisible(true)} />
 
-      <Modal
-        open={visible}
-        styles={{ maxWidth: 1300 }}
-        onClose={() => setVisible(false)}
-      >
-        <h2>{`Adicionar Item ao ${getWord("Orçamento")} - ${budgetTag}`}</h2>
-        <Negotiation budgetId={budgetId} onUpdatePayment={onUpdatePayment} />
-      </Modal>
-    </>
-  );
+			<Modal
+				open={visible}
+				styles={{ maxWidth: 1300 }}
+				onClose={() => setVisible(false)}
+			>
+				<h2>{`Adicionar Item ao ${getWord("Orçamento")} - ${budgetTag}`}</h2>
+				<Negotiation budgetId={budgetId} onUpdatePayment={onUpdatePayment} />
+			</Modal>
+		</>
+	);
 }
