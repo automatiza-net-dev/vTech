ad8db0fe7f3af78f944b2bfdd108262e48ca04f9
STNeto1 <gfreitasneto18@gmail.com>
2025-09-03 14:50:47 -0300

    feat: popup and stuff

--- src/OLD/components/Attendance/Timeline/LaunchedVaccinesList/DosesModal/FormChild.tsx
+++ src/OLD/components/Attendance/Timeline/LaunchedVaccinesList/DosesModal/FormChild.tsx
@@ -1,14 +1,14 @@
-import React, { memo, useEffect } from "react";
+import React, { useEffect } from "react";
 
-import { TbVaccine, TbEraser } from "react-icons/tb";
+import { TbVaccine, TbEraser, TbXboxX } from "react-icons/tb";
 
 import {
-	Icon,
 	useToast,
 	Button,
 	useMutation,
 	useQueryClient,
 	Popconfirm,
+	Tooltip,
 } from "infinity-forge";
 
 import { Checkbox, Input } from "antd";
@@ -315,138 +315,156 @@ export default function FormChild({
 									}`}
 									style={{ gap: "8px" }}
 								>
-									<span>
-										<TbVaccine
-											size={20}
-											className={
-												!item?.applicationDate
-													? !actionState
-														? "vaccine-icon"
+									<Tooltip
+										idTooltip="action"
+										enableHover
+										position="top-center"
+										content={"Aplicar vacina"}
+										trigger={
+											<TbVaccine
+												size={20}
+												className={
+													!item?.applicationDate
+														? !actionState
+															? "vaccine-icon"
+															: "icon-inative"
 														: "icon-inative"
-													: "icon-inative"
-											}
-											onClick={() => {
-												if (!item?.applicationDate) {
-													setSelectedIndex(i);
-													setActualData({ ...item });
-													setActionState("vaccine");
 												}
-											}}
-										/>
-									</span>
-									<span>
-										<svg
-											onClick={() => {
-												if (!item?.applicationDate) {
-													setSelectedIndex(i);
-													setActualData({ ...item });
-													setActionState("schedule");
-												}
-											}}
-											viewBox="0 0 16 16"
-											height="15"
-											width="15"
-											focusable="false"
-											role="img"
-											fill="#000"
-											xmlns="http://www.w3.org/2000/svg"
-											className={
-												!item?.applicationDate
-													? !actionState
-														? "schedule-active"
-														: "icon-inative"
-													: "icon-inative"
-											}
-										>
-											<title>Calendar icon</title>
-											<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"></path>
-										</svg>
-									</span>
-									<span>
-										<svg
-											onClick={() => {
-												const obj = [...calendars];
-												obj.splice(i, 1, {
-													...item,
-													...actualData,
-												});
-												if (
-													!actualData?.batch ||
-													!actualData?.applicationDate ||
-													!actualData?.laboratory
-												) {
-													return createToast({
-														message:
-															"verifique se os campos estão preenchidos corretamente",
-														status: "error",
-													});
-												}
+												onClick={() => {
+													if (!item?.applicationDate) {
+														setSelectedIndex(i);
+														setActualData({ ...item });
+														setActionState("vaccine");
+													}
+												}}
+											/>
+										}
+									/>
 
-												if (actionState) {
-													submitUpdate(actualData);
-													setCalendars(obj);
+									<Tooltip
+										idTooltip="action"
+										enableHover
+										position="top-center"
+										content={"Reagendar vacina"}
+										trigger={
+											<svg
+												onClick={() => {
+													if (!item?.applicationDate) {
+														setSelectedIndex(i);
+														setActualData({ ...item });
+														setActionState("schedule");
+													}
+												}}
+												viewBox="0 0 16 16"
+												height="15"
+												width="15"
+												focusable="false"
+												role="img"
+												fill="#000"
+												xmlns="http://www.w3.org/2000/svg"
+												className={
+													!item?.applicationDate
+														? !actionState
+															? "schedule-active"
+															: "icon-inative"
+														: "icon-inative"
+												}
+											>
+												<title>Calendar icon</title>
+												<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"></path>
+											</svg>
+										}
+									/>
+
+									<Tooltip
+										idTooltip="action"
+										enableHover
+										position="top-center"
+										content={"Confirmar aplicação"}
+										trigger={
+											<svg
+												onClick={() => {
+													const obj = [...calendars];
+													obj.splice(i, 1, {
+														...item,
+														...actualData,
+													});
+													if (
+														!actualData?.batch ||
+														!actualData?.applicationDate ||
+														!actualData?.laboratory
+													) {
+														return createToast({
+															message:
+																"verifique se os campos estão preenchidos corretamente",
+															status: "error",
+														});
+													}
+
+													if (actionState) {
+														submitUpdate(actualData);
+														setCalendars(obj);
+														setSelectedIndex(false);
+														setActionState(false);
+														setActualData({});
+													}
+												}}
+												viewBox="0 0 16 16"
+												height="20"
+												width="20"
+												focusable="false"
+												role="img"
+												fill="currentColor"
+												xmlns="http://www.w3.org/2000/svg"
+												className={
+													actionState && selectedIndex === i
+														? "confirm-icon-active"
+														: "icon-inative"
+												}
+											>
+												<title>CheckCircle icon</title>
+												<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+												<path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
+											</svg>
+										}
+									/>
+
+									<Tooltip
+										idTooltip="action"
+										enableHover
+										position="top-center"
+										content={"Limpar dados da aplicação"}
+										trigger={
+											<TbEraser
+												size={"24px"}
+												onClick={() => {
+													clearVaccineRecord.mutate(item.id);
+												}}
+											/>
+										}
+									/>
+
+									<Tooltip
+										idTooltip="action"
+										enableHover
+										position="top-center"
+										content={"Cancelar aplicação"}
+										trigger={
+											<TbXboxX
+												size={"24px"}
+												onClick={() => {
 													setSelectedIndex(false);
 													setActionState(false);
 													setActualData({});
+												}}
+												className={
+													actionState && selectedIndex === i
+														? "confirm-icon-active"
+														: "icon-inative"
 												}
-											}}
-											viewBox="0 0 16 16"
-											height="20"
-											width="20"
-											focusable="false"
-											role="img"
-											fill="currentColor"
-											xmlns="http://www.w3.org/2000/svg"
-											className={
-												actionState && selectedIndex === i
-													? "confirm-icon-active"
-													: "icon-inative"
-											}
-										>
-											<title>CheckCircle icon</title>
-											<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-											<path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
-										</svg>
-									</span>
-
-									<span>
-										<button
-											type="button"
-											onClick={() => {
-												clearVaccineRecord.mutate(item.id);
-											}}
-											style={{
-												background: "transparent",
-												border: 0,
-												padding: 0,
-											}}
-										>
-											<TbEraser size={"20px"} />
-										</button>
-									</span>
-
-									<span>
-										<button
-											type="button"
-											className={
-												actionState && selectedIndex === i
-													? "cancel-icon-active"
-													: "icon-inative"
-											}
-											onClick={() => {
-												setSelectedIndex(false);
-												setActionState(false);
-												setActualData({});
-											}}
-											style={{
-												background: "transparent",
-												border: 0,
-												padding: 0,
-											}}
-										>
-											<Icon name="IconBlock" color="#f10" />
-										</button>
-									</span>
+											/>
+										}
+									/>
 								</div>
 							))}
 					</div>
