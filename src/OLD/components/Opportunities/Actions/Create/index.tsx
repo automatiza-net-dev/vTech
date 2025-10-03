e32c7c7b9e1c3b9a9275559f7861fac165fcc145
STNeto1 <gfreitasneto18@gmail.com>
2025-10-03 08:19:47 -0300

    feat: loading and stuff

--- src/OLD/components/Opportunities/Actions/Create/index.tsx
+++ src/OLD/components/Opportunities/Actions/Create/index.tsx
@@ -20,151 +20,157 @@ import { currencyFormatter } from "@/OLD/components/Budget";
 import { convertIntlCurrency } from "@/OLD/utils/convertIntl";
 
 export default function Create({
-  clients,
-  colaborators,
-  crmStatus,
-  contactTypes,
-  subjects,
+	clients,
+	colaborators,
+	crmStatus,
+	contactTypes,
+	subjects,
 }) {
-  const [data, setData] = useState<any>({});
-  const [patientListVisible, setPatientListVisible] = useState(true);
-  const [reload, setReload] = useState(false);
+	const [data, setData] = useState<any>({});
+	const [patientListVisible, setPatientListVisible] = useState(true);
+	const [reload, setReload] = useState(false);
 
-  const { user } = useAuthAdmin();
-  const { clinic } = useProfile();
-  const { crmData, setCrmData } = useAuth();
+	const { user } = useAuthAdmin();
+	const { clinic } = useProfile();
+	const { crmData, setCrmData } = useAuth();
 
-  const { createToast } = useToast();
+	const { createToast } = useToast();
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const {type} = useConfigurationsSystem()
+	const { type } = useConfigurationsSystem();
 
-  async function createOpportunity() {
+	const [creatingOpp, setCreatingOpp] = useState(false);
+	useEffect(() => {
+		console.log({ creatingOpp });
+	}, [creatingOpp]);
+	async function createOpportunity() {
+		setCreatingOpp(true);
 
-    const newObj = {
-      ...data,
-      castrated: data?.castrated ? JSON.parse(data?.castrated) : "false",
-      businessUnitId: clinic?.id,
-      contactDate: moment(data?.contactDate).toISOString(),
-      value: convertIntlCurrency(data?.value),
-      clientId: type === "Vet" ?  data?.patient_id || data?.clientId : undefined
-    };
+		const newObj = {
+			...data,
+			castrated: data?.castrated ? JSON.parse(data?.castrated) : "false",
+			businessUnitId: clinic?.id,
+			contactDate: moment(data?.contactDate).toISOString(),
+			value: convertIntlCurrency(data?.value),
+			clientId: type === "Vet" ? data?.patient_id || data?.clientId : undefined,
+		};
 
-    if (newObj.clientId === "-") {
-      delete newObj.clientId;
-      delete newObj.castrated;
-    }
+		if (newObj.clientId === "-") {
+			delete newObj.clientId;
+			delete newObj.castrated;
+		}
 
-    try {
-      await opportunitiesService.create(newObj);
+		try {
+			await opportunitiesService.create(newObj);
 
-      createToast({
-        status: "success",
-        message: "Oportunidade cadastrada com sucesso!",
-      });
+			createToast({
+				status: "success",
+				message: "Oportunidade cadastrada com sucesso!",
+			});
 
-      router.back();
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        return createToast({
-          status: "error",
-          message: error?.response?.data?.title,
-        });
-      }
+			router.back();
+		} catch (error) {
+			if (error instanceof AxiosError) {
+				return createToast({
+					status: "error",
+					message: error?.response?.data?.title,
+				});
+			}
 
-      return createToast({
-        status: "error",
-        message: "Verifique os campos informados...",
-      });
-    }
-  }
+			return createToast({
+				status: "error",
+				message: "Verifique os campos informados...",
+			});
+		} finally {
+			setCreatingOpp(false);
+		}
+	}
 
-  useEffect(() => {
-    setData({
-      ...data,
-      contactDate: moment(new Date()),
-      value: currencyFormatter(0),
-      userId: user?.id,
-      collabName: user?.name,
-      statusId: crmStatus?.find((st) => st?.tag === "N")?.id,
-    });
-  }, [user, crmStatus]);
+	useEffect(() => {
+		setData({
+			...data,
+			contactDate: moment(new Date()),
+			value: currencyFormatter(0),
+			userId: user?.id,
+			collabName: user?.name,
+			statusId: crmStatus?.find((st) => st?.tag === "N")?.id,
+		});
+	}, [user, crmStatus]);
 
-  useEffect(() => {
-    if (crmData) {
-      setData(crmData);
-      setCrmData(false);
-      setPatientListVisible(false);
-    }
-  }, []);
+	useEffect(() => {
+		if (crmData) {
+			setData(crmData);
+			setCrmData(false);
+			setPatientListVisible(false);
+		}
+	}, []);
 
-  useEffect(() => {
-    setReload((prv) => !prv);
-  }, [patientListVisible]);
+	useEffect(() => {
+		setReload((prv) => !prv);
+	}, [patientListVisible]);
 
+	return (
+		<PageWrapper title="Nova oportunidade">
+			<Container>
+				<hr />
+				{!patientListVisible && (
+					<FormChild
+						data={data}
+						setData={setData}
+						submit={createOpportunity}
+						reload={reload}
+						clients={clients}
+						colaborators={colaborators}
+						crmStatus={crmStatus}
+						contactTypes={contactTypes}
+						subjects={subjects}
+						creatingOpportunity={creatingOpp}
+					/>
+				)}
 
-
-  return (
-    <PageWrapper title="Nova oportunidade">
-      <Container>
-        <hr />
-        {!patientListVisible && (
-          <FormChild
-            data={data}
-            setData={setData}
-            submit={createOpportunity}
-            reload={reload}
-            clients={clients}
-            colaborators={colaborators}
-            crmStatus={crmStatus}
-            contactTypes={contactTypes}
-            subjects={subjects}
-          />
-        )}
-
-        {type === "Vet" && (
-          <Modal
-            title={"Selecionar paciente"}
-            width={1200}
-            visible={patientListVisible}
-            onCancel={() => setPatientListVisible(false)}
-            footer={null}
-          >
-            <Patient
-              setPayload={setData}
-              payload={data}
-              modal={true}
-              setVisible={setPatientListVisible}
-              setSearch={setData}
-              patientListVisible={patientListVisible}
-              setReload={setReload}
-              origin="opportunities"
-            />
-          </Modal>
-        )}
-        {type !== "Vet" && (
-          <Modal
-            title={"Selecionar Cliente"}
-            width={1200}
-            visible={patientListVisible}
-            onCancel={() => setPatientListVisible(false)}
-            footer={null}
-          >
-            <Tutor
-              setPayload={setData}
-              payload={data}
-              modal={true}
-              setVisible={setPatientListVisible}
-              setSearch={setData}
-              patientListVisible={patientListVisible}
-              setReload={setReload}
-              reload={reload}
-              origin="opportunities"
-            />
-          </Modal>
-        )}
-      </Container>
-    </PageWrapper>
-  );
+				{type === "Vet" && (
+					<Modal
+						title={"Selecionar paciente"}
+						width={1200}
+						visible={patientListVisible}
+						onCancel={() => setPatientListVisible(false)}
+						footer={null}
+					>
+						<Patient
+							setPayload={setData}
+							payload={data}
+							modal={true}
+							setVisible={setPatientListVisible}
+							setSearch={setData}
+							patientListVisible={patientListVisible}
+							setReload={setReload}
+							origin="opportunities"
+						/>
+					</Modal>
+				)}
+				{type !== "Vet" && (
+					<Modal
+						title={"Selecionar Cliente"}
+						width={1200}
+						visible={patientListVisible}
+						onCancel={() => setPatientListVisible(false)}
+						footer={null}
+					>
+						<Tutor
+							setPayload={setData}
+							payload={data}
+							modal={true}
+							setVisible={setPatientListVisible}
+							setSearch={setData}
+							patientListVisible={patientListVisible}
+							setReload={setReload}
+							reload={reload}
+							origin="opportunities"
+						/>
+					</Modal>
+				)}
+			</Container>
+		</PageWrapper>
+	);
 }
