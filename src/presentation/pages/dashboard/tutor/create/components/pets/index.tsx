d3def22e5c001393670cf6fafcf5135826aeb4c4
STNeto1 <gfreitasneto18@gmail.com>
2025-10-16 12:34:39 -0300

    fix: filter only pets

--- src/presentation/pages/dashboard/tutor/create/components/pets/index.tsx
+++ src/presentation/pages/dashboard/tutor/create/components/pets/index.tsx
@@ -14,6 +14,7 @@ import { useRouter } from "next/router";
 export function Pets({
 	addPet,
 	handleSuccess,
+	tutorId,
 }: {
 	tutor?: Tutor;
 	handleSuccess?(data: any): Promise<void>;
@@ -26,7 +27,7 @@ export function Pets({
 	);
 
 	const patientFilters = {
-		fetch: modalAddPet,
+		tutorID: tutorId,
 	};
 
 	const { createToast } = useToast();
