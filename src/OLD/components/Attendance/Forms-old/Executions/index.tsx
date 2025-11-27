cc2a8318d0628c91eec1869007a62b0a22c5e1a2
STNeto1 <gfreitasneto18@gmail.com>
2025-11-27 15:48:28 -0300

    fix: holder

--- src/OLD/components/Attendance/Forms-old/Executions/index.tsx
+++ src/OLD/components/Attendance/Forms-old/Executions/index.tsx
@@ -1,93 +1,101 @@
 import Maintenance from "@/OLD/components/Treatments/Maintenance";
 import { treatmentService } from "@/OLD/services/treatments.service";
+import { useLoadPatient } from "@/presentation";
 import { Table } from "antd";
 import { useQuery } from "infinity-forge";
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
 
 export default function Executions(props: {
-	value: "Execução de Tratamento";
-	modal: boolean;
-	reloadSchedule: () => void;
-	setModal: (value: boolean) => void;
+  value: "Execução de Tratamento";
+  modal: boolean;
+  reloadSchedule: () => void;
+  setModal: (value: boolean) => void;
 }) {
-	const params = useParams();
-	const patientID = params?.id;
+  const params = useParams();
 
-	const [selected, setSelected] = useState<number | null>(null);
+  const patientID = params?.id;
+  const patient = useLoadPatient(patientID as string | undefined);
+  const holderID =
+    // @ts-expect-error main
+    patient.data?.holders?.find((h) => h?.main)?.id ||
+    patient.data?.holders?.at(0)?.id;
 
-	const searchQuery = useQuery({
-		enabled: !!patientID && props.modal,
-		queryKey: ["search-not-executed", patientID],
-		queryFn: async () => {
-			const response = await treatmentService.searchNotExecuted({
-				client: patientID,
-			});
+  const [selected, setSelected] = useState<number | null>(null);
 
-			return response.data;
-		},
-	});
+  const searchQuery = useQuery({
+    enabled: !!patientID && props.modal,
+    queryKey: ["search-not-executed", patientID, holderID],
+    queryFn: async () => {
+      const response = await treatmentService.searchNotExecuted({
+        client: patientID,
+        holder: holderID,
+      });
 
-	if (searchQuery.isLoading || !props.modal) return <p>Loading...</p>;
+      return response.data;
+    },
+  });
 
-	if (searchQuery.data.length === 0) {
-		return <p>Não há dados</p>;
-	}
+  if (searchQuery.isLoading || !props.modal) return <p>Loading...</p>;
 
-	if (searchQuery.data.length === 1) {
-		return (
-			<Maintenance
-				treatmentId={searchQuery.data[0].id}
-				close={() => props.setModal(false)}
-			/>
-		);
-	}
+  if (searchQuery.data.length === 0) {
+    return <p>Não há dados</p>;
+  }
 
-	if (!!selected) {
-		return (
-			<Maintenance treatmentId={selected} close={() => props.setModal(false)} />
-		);
-	}
+  if (searchQuery.data.length === 1) {
+    return (
+      <Maintenance
+        treatmentId={searchQuery.data[0].id}
+        close={() => props.setModal(false)}
+      />
+    );
+  }
 
-	return (
-		<div className="uk-flex-column uk-flex-between">
-			<p
-				style={{
-					fontSize: "20px",
-					color: "black",
-					fontWeight: "semibold",
-				}}
-			>
-				Selecione o tratamento a ser executado
-			</p>
-			<hr />
-			<Table
-				className="uk-margin-top"
-				dataSource={searchQuery.data.map((d) => ({
-					tID: (
-						<span
-							style={{
-								cursor: "pointer",
-							}}
-							onClick={() => setSelected(d.id)}
-						>{`${d.id} - ${d.items.length} itens`}</span>
-					),
-					date: new Date(d.date).toLocaleDateString(),
-				}))}
-				columns={[
-					{
-						title: "Tratamento",
-						dataIndex: "tID",
-						key: "tID",
-					},
-					{
-						title: "Data",
-						dataIndex: "date",
-						key: "date",
-					},
-				]}
-				locale={{}}
-			/>
-		</div>
-	);
+  if (!!selected) {
+    return (
+      <Maintenance treatmentId={selected} close={() => props.setModal(false)} />
+    );
+  }
+
+  return (
+    <div className="uk-flex-column uk-flex-between">
+      <p
+        style={{
+          fontSize: "20px",
+          color: "black",
+          fontWeight: "semibold",
+        }}
+      >
+        Selecione o tratamento a ser executado
+      </p>
+      <hr />
+      <Table
+        className="uk-margin-top"
+        dataSource={searchQuery.data.map((d) => ({
+          tID: (
+            <span
+              style={{
+                cursor: "pointer",
+              }}
+              onClick={() => setSelected(d.id)}
+            >{`${d.id} - ${d.items.length} itens`}</span>
+          ),
+          date: new Date(d.date).toLocaleDateString(),
+        }))}
+        columns={[
+          {
+            title: "Tratamento",
+            dataIndex: "tID",
+            key: "tID",
+          },
+          {
+            title: "Data",
+            dataIndex: "date",
+            key: "date",
+          },
+        ]}
+        locale={{}}
+      />
+    </div>
+  );
 }
